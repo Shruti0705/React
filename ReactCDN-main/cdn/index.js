f7e0b440dad6446353e5c73fd51a0cfd0ac7512e
Shruti <120258766+Shruti0705@users.noreply.github.com>
2024-02-14 23:40:38 +0530

    Add files via upload

---
+++ ReactCDN-main/cdn/index.js
@@ -0,0 +1,12 @@
+function App()
+{
+    return(
+        <h1> Hello World </h1>
+    )
+}
+
+
+
+//ReactDOM.render(<h1> Hello World </h1>,document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(h1);
